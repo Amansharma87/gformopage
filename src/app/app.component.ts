c4ac52c52da7dc1d8fe26be8a73056e7d6ae93b8
Amansharma87 <aman87337@gmail.com>
2020-03-29 17:40:04 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, Output, Input } from '@angular/core';
+import {  HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'generic-form-page';
+export class AppComponent implements OnInit{
+  [x: string]: any;
+  d:any;
+ 
+  server="false";
+  constructor(private http: HttpClient){
+    
+  }
+  getoptions(selecteditem:any){
+   this.d=selecteditem.target.value;
+    console.log(selecteditem.target.value);
+  }
+  ngOnInit(){
+    
+      this.http.get('https://ca.platform.simplifii.xyz/api/v1/static/assignment4')
+      .subscribe(
+        (response:any)=>{
+          
+          const data=response.response.data;
+          console.log(data);
+          this.d=data;
+        },
+     
+  
+    );
+  
+     };
+  
+
+ 
 }
